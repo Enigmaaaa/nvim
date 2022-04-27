4fc75ffc893d89dd2b6654c9cbb86b05697b8ff6
Enigmaaaa <wenzhe2000623@outlook.com>
2022-04-27 11:41:39 +0800

    using git to install config

--- init.ps1
+++ init.ps1
@@ -1,5 +1,5 @@
 if (!(Test-Path ~\AppData\Local\nvim)) {
     git clone https://github.com/Enigmaaaa/nvim.git ~\AppData\Local\nvim
-    Copy-Item -Path $PSScriptRoot -Destination ~\AppData\Local\nvim -Recurse -Exclude 'plugged'
+    # Copy-Item -Path $PSScriptRoot -Destination ~\AppData\Local\nvim -Recurse -Exclude 'plugged'
     Write-Host 'Neovim init done'
 }
